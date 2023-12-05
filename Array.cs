2c53219028122747e8a41a4616555580f0f27081
ShatiCK <152949805+ShatiCK@users.noreply.github.com>
2023-12-05 13:03:18 +0700

    Добавьте файлы проекта.

---
+++ Array.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice4
+{
+    public class Array
+    {
+        private int[] array;
+        public int Length => array.Length;
+
+        // Конструктор
+        public void IntegerArray(int size)
+        {
+            if (size < 0)
+            {
+                throw new ArgumentException("Размер должен быть неотрицательным.");
+            }
+            array = new int[size];
+        }
+
+        // Индексатор
+        public int this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= array.Length)
+                {
+                    throw new IndexOutOfRangeException("Индекс вне диапазона.");
+                }
+                return array[index];
+            }
+            set
+            {
+                if (index < 0 || index >= array.Length)
+                {
+                    throw new IndexOutOfRangeException("Индекс вне диапазона.");
+                }
+                array[index] = value;
+            }
+        }
+    }
+}
+
